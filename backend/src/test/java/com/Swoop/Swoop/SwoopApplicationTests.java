b70967c34571415b56116d74bb4afc12d4862aa7
CCLDArjun <ccldarjun@icloud.com>
2022-11-14 17:17:32 -0800

    first commit of backend

---
+++ backend/src/test/java/com/Swoop/Swoop/SwoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Swoop.Swoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
