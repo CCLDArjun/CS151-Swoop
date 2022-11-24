df7c2abd61ac7ddab2c3deced19aa34dbe9376b1
odiaz95012 <odiaz95012@gmail.com>
2022-11-24 15:16:50 -0800

    Created account info page

--- swoop/src/viewProfileViews/ViewProfile.js
+++ swoop/src/viewProfileViews/ViewProfile.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navbar } from "../components/reactMenu/navbar";
 import { Text } from "@react-ui-org/react-ui";
 import styled from "styled-components";
 import "./ViewProfileStyles.css";
 import { Helmet } from "react-helmet";
 import MovingComponent from "react-moving-text";
+import {AwesomeButton} from "react-awesome-button";
+import AwesomeButtonStyles from "react-awesome-button";
+import { CgProfile } from "react-icons/cg";
+import axios from "axios";
 
 function ViewProfile() {
+  var retrievedEmail = localStorage.getItem('email');
+  var formattedEmail = retrievedEmail.replaceAll('"', '');
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  async function getUser(email) {
+    try{
+      axios.get('http://localhost:8080/api/v1/user/' + email)
+      .then(function (response){
+        setName(response.data.fullName);
+        setPassword(response.data.password);
+        return response;
+      })
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+
   return (
     <div id="container">
       <Helmet>
@@ -29,21 +51,36 @@ function ViewProfile() {
           <div id="accountInfoHeader">
             <h1>Account Information</h1>
           </div>
-        </div>
-      </MovingComponent>
-      <MovingComponent
-        type="fadeInFromRight"
-        duration="2000ms"
-        delay="0s"
-        direction="normal"
-        timing="ease-in"
-        iteration="1"
-        fillMode="none"
-      >
-        <div class="infoContainer">
-          <div id="recentTripsHeader">
-            <h1>Recent Trips</h1>
+          <div id="accountInfo">
+            <div className="userInfo" >Name: {name}</div>
+            <div className="userInfo">Email: {formattedEmail}</div>
+            <div className="userInfo">Password: {password}</div>
           </div>
+        </div>
+        <div id="displayButton">
+          <MovingComponent
+            type="fadeInFromLeft"
+            duration="2000ms"
+            delay="0s"
+            direction="normal"
+            timing="ease-in"
+            iteration="1"
+            fillMode="none"
+          >
+            <AwesomeButton
+              cssModule={AwesomeButtonStyles}
+              before={<CgProfile/>}
+              type="primary"
+              size="medium"
+              onPress={(event) => {
+                getUser(formattedEmail);
+                document.getElementById("accountInfo").style.visibility =
+                  "visible";
+              }}
+            >
+              Display
+            </AwesomeButton>
+          </MovingComponent>
         </div>
       </MovingComponent>
     </div>
