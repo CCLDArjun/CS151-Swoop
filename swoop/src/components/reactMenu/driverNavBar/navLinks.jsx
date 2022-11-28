6b57016cd38272820871b0aeac501bc8577a881f
odiaz95012 <odiaz95012@gmail.com>
2022-11-27 22:26:15 -0800

    Fixed View Trips Page

--- swoop/src/components/reactMenu/driverNavBar/navLinks.jsx
+++ swoop/src/components/reactMenu/driverNavBar/navLinks.jsx
@@ -13,6 +13,7 @@ const LinksWrapper = styled.ul`
   display: flex;
   height: 100%;
   list-style: none;
+  margin-left: -100px;
 `;
 
 const LinkItem = styled.li`
@@ -49,7 +50,7 @@ export function NavLinks(props) {
           <Link href="/driver/viewTrips">View Trips</Link>
         </LinkItem>
         <LinkItem>
-          <Link href="/rider/viewImpact">View Carpool Requests</Link>
+          <Link href="/driver">Plan Trip</Link>
         </LinkItem>
       </LinksWrapper>
     </NavLinksContainer>
--- swoop/src/viewTripsViews/viewTrips.js
+++ swoop/src/viewTripsViews/viewTrips.js
@@ -1,48 +1,81 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import "./viewTripsStyles.css";
 import { DriverNavbar } from "../components/reactMenu/driverNavBar";
 import { Helmet } from "react-helmet";
 function ViewTrips() {
   const userEmail = localStorage.getItem("email").replaceAll('"', "");
-  var tripIds = []; // Store the whole trip object so that we have 
+  const [location, setLocation] = useState("");
+  var tripIds = []; // Store the whole trip object so that we have
+  var tripStartAndEndLocations = [];
   function getTrips() {
     axios.get("http://localhost:8080/api/v1/rider/rides").then((response) => {
       console.log(response);
-      Object.keys(response.data).forEach(trip => {
+      Object.keys(response.data).forEach((trip) => {
         tripIds.push(response.data[trip]);
       });
       console.log(tripIds);
     });
   }
-  function createTripContainers(tripsList){
-    for(var i = 0; i < tripsList.length; i++){
+  function createTripContainers(tripsList) {
+    for (var i = 0; i < tripsList.length; i++) {
       const newTripContainer = document.createElement("div");
-      newTripContainer.className = "tripContainer"
+      newTripContainer.className = "tripContainer";
       const newContent = document.createTextNode(`Trip Details`);
       newTripContainer.appendChild(newContent);
       document.getElementById("allTripsContainer").appendChild(newTripContainer);
     }
   }
-  function populateTripContainers(tripsList){
+
+  function populateTripContainers(tripsList) {
     //tripsList holds the data for each trip
     const parentObject = document.getElementsByClassName("tripContainer");
-    [...parentObject].forEach((parent, i) =>{
+    [...parentObject].forEach((parent, i) => {
+      //Show trip id's
       const tripIdContainer = document.createElement("div");
       tripIdContainer.className = "tripInfoContainer";
-      tripIdContainer.innerHTML = `Trip ID: ${tripsList[i].id}`
+      tripIdContainer.innerHTML = `Trip ID: ${tripsList[i].id}`;
       parent.appendChild(tripIdContainer);
-    })
+
+      const originContainer = document.createElement("div");
+      originContainer.className = "tripInfoContainer";
+      var start = tripsList[i].start;
+      if(start.includes("-")){
+        console.log("SPACE FOUND IN START");
+        start = start.replace("-", " ");
+      }
+      originContainer.innerHTML = `Trip Origin: ${start}`;
+      parent.appendChild(originContainer);
+      var end = tripsList[i].end;
+      if(end.includes("-")){
+        end = end.replace("-", " ");
+      }
+      const destContainer = document.createElement("div");
+      destContainer.className = "tripInfoContainer";
+      destContainer.innerHTML = `Trip Destination: ${end}`;
+      parent.appendChild(destContainer);
+
+      const distanceContainer = document.createElement("div");
+      distanceContainer.className = "tripInfoContainer";
+      distanceContainer.innerHTML = `Total Trip Distance: ${Math.ceil(tripsList[i].distance) + ' miles'}`;
+      parent.appendChild(distanceContainer);
+
+      const co2Container = document.createElement("div");
+      co2Container.className = "tripInfoContainer";
+      co2Container.innerHTML = `Carbon Footprint: ${Math.ceil(tripsList[i].CO2) + ' pounds'}`;
+      parent.appendChild(co2Container);
+    });
   }
-  window.onload = function(){
+
+  window.onload = function () {
     getTrips();
-    setTimeout(function(){
+    setTimeout(function () {
       createTripContainers(tripIds);
     }, 500);
-    setTimeout(function(){
+    setTimeout(function () {
       populateTripContainers(tripIds);
-    },1000);
-  }
+    }, 1000);
+  };
   //TODO: Create a function that populates each trip container with its respective trip
   return (
     <div className="container">
@@ -52,9 +85,9 @@ function ViewTrips() {
         <link rel="canonical" href="/home" />
         <meta name="description" content="Swoop View Impact Page" />
       </Helmet>
-      <DriverNavbar/>
+      <DriverNavbar />
       <div id="allTripsContainer">
-
+        
       </div>
     </div>
   );
