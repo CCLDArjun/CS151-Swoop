f5e2a6a9d0709a92981fc83c0712a5dc36f09346
odiaz95012 <odiaz95012@gmail.com>
2022-11-29 20:05:21 -0800

    Added password validation to the sign up page.

--- swoop/src/components/accountBox/signupForm.jsx
+++ swoop/src/components/accountBox/signupForm.jsx
@@ -14,44 +14,72 @@ import axios from "axios";
 export function SignupForm(props) {
   const { switchToSignin } = useContext(AccountContext);
   const sendUserToBackEnd = () => {
-    var name = document.getElementById('signUpNameField').value;
-    var email = document.getElementById('signUpEmailField').value;
-    var firstPassword = document.getElementById('signUpPasswordField').value;
-    var confirmPassword = document.getElementById('signUpConfirmPasswordField').value;
-    if(firstPassword === confirmPassword){
-      try{
-        const res = axios.post('http://localhost:8080/api/v1/user',{
+    var name = document.getElementById("signUpNameField").value;
+    var email = document.getElementById("signUpEmailField").value;
+    var firstPassword = document.getElementById("signUpPasswordField").value;
+    var confirmPassword = document.getElementById(
+      "signUpConfirmPasswordField"
+    ).value;
+    if (firstPassword === confirmPassword && CheckPassword(firstPassword) === true) {
+      try {
+        const res = axios.post("http://localhost:8080/api/v1/user", {
           fullName: name,
           email: email,
-          password: confirmPassword
-        })
+          password: confirmPassword,
+        });
         switchToSignin();
-      } catch(err){
+      } catch (err) {
         console.log(err);
       }
-    }else{
-      alert('Passwords did not match.')
+    }else if(CheckPassword(firstPassword) === false){
+      alert('The inputted password did not contain one of the following: 6-20 characters, at least one numeric digit, one uppercase, and one lowercase letter.')
+    } else {
+      alert("Passwords did not match.");
     }
-
+  };
+  function CheckPassword(inputtxt) {
+    var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+    if (inputtxt.match(passw)) {
+      return true;
+    } else {
+      return false;
+    }
   }
+
   const [currentUserEmail, setUserEmail] = useState(() => {
     const savedEmail = localStorage.getItem("email");
     const initialValue = savedEmail;
     return initialValue || "";
   });
+
   useEffect(() => {
-    localStorage.setItem("email", JSON.stringify(currentUserEmail))
+    localStorage.setItem("email", JSON.stringify(currentUserEmail));
   }, [currentUserEmail]);
   return (
     <BoxContainer>
       <FormContainer>
-        <Input id = "signUpNameField" type="text" placeholder="Full Name" />
-        <Input id = "signUpEmailField" type="email" placeholder="Email" onChange={(e) => setUserEmail(e.target.value)}/>
-        <Input id = "signUpPasswordField" type="password" placeholder="Password" />
-        <Input id = "signUpConfirmPasswordField" type="password" placeholder="Confirm Password" />
+        <Input id="signUpNameField" type="text" placeholder="Full Name" />
+        <Input
+          id="signUpEmailField"
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setUserEmail(e.target.value)}
+        />
+        <Input
+          id="signUpPasswordField"
+          type="password"
+          placeholder="Password"
+        />
+        <Input
+          id="signUpConfirmPasswordField"
+          type="password"
+          placeholder="Confirm Password"
+        />
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
-      <SubmitButton type="submit" onClick={sendUserToBackEnd}>Signup</SubmitButton>
+      <SubmitButton type="submit" onClick={sendUserToBackEnd}>
+        Signup
+      </SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Already have an account?
